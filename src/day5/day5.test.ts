e907e673308327df8e9c7d0de647c13c8836f838
Ludek Stepan <ludek.stepan@gmail.com>
2020-12-05 08:09:06 +0100

    Day 5

--- src/day5/day5.test.ts
+++ src/day5/day5.test.ts
@@ -1,11 +1,17 @@
-import { stage1, stage2 } from './index'
+import { getId, stage1, stage2 } from './index'
 
 describe('Day 5', () => {
+    test('Example', () => {
+        expect(getId('BFFFBBFRRR')).toBe(567)
+        expect(getId('FFFBBBFRRR')).toBe(119)
+        expect(getId('BBFFBBFRLL')).toBe(820)
+    })
+
     test('Stage 1', () => {
-        expect(stage1()).toBe(undefined)
+        expect(stage1()).toBe(930)
     })
 
     test('Stage 2', () => {
-        expect(stage2()).toBe(undefined)
+        expect(stage2()).toBe(515)
     })
 })
--- src/day5/index.ts
+++ src/day5/index.ts
@@ -1,4 +1,5 @@
 import input from './input.txt'
+
 import { notEmptyString } from '../utils'
 
 function parse(file: string) {
@@ -6,10 +7,46 @@ function parse(file: string) {
     return lines
 }
 
+export function getId(pass: string) {
+    let row = 0,
+        column = 0
+
+    for (const char of pass) {
+        switch (char) {
+            case 'F':
+                row <<= 1
+                break
+            case 'B':
+                row <<= 1
+                row |= 1
+                break
+            case 'L':
+                column <<= 1
+                break
+            case 'R':
+                column <<= 1
+                column |= 1
+                break
+        }
+    }
+
+    return row * 8 + column
+}
+
 export function stage1() {
-    return
+    return parse(input)
+        .map(getId)
+        .reduce((a, b) => Math.max(a, b))
 }
 
 export function stage2() {
-    return
+    let seats = parse(input)
+        .map(getId)
+        .sort((a, b) => a - b)
+
+    for (let id = seats[0]; id < seats[0] + seats.length; id++) {
+        if (seats.includes(id) && !seats.includes(id + 1) && seats.includes(id + 2)) {
+            return id + 1
+        }
+    }
 }
